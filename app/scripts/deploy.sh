dea0f56f1884401a6b870035320c31c82d3b87a5
sensundi <sen.sundi@gmail.com>
2021-11-24 00:31:15 +1100

    Updates to cfn stacks, params and deploy script that deploys the stacks

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -10,4 +10,5 @@ export AWS_SECRET_ACCESS_KEY=$(aws sts assume-role --role-arn "arn:aws:iam::1234
 export AWS_ACCESS_KEY_ID=$(aws sts assume-role --role-arn "arn:aws:iam::123456789012:role/cross-account-role" --role-session-name AWSCLI-Session  | jq -r '.Credentials[] | .AccessKeyId'
 export AWS_SESSION_TOKEN=$(aws sts assume-role --role-arn "arn:aws:iam::123456789012:role/cross-account-role" --role-session-name AWSCLI-Session  | jq -r '.Credentials[] | .SessionToken'
 
-cat ${ROOT_DIR}/parameters/dev-app-service | xargs aws cloudformation deploy --template-file ${ROOT_DIR}/cloudformation/app-stack.cf.yaml --capabilities CAPABILITY_IAM
+cat ${ROOT_DIR}/parameters/${STAGE}-app-service | xargs aws cloudformation deploy --template-file ${ROOT_DIR}/cloudformation/app-service.cf.yaml --capabilities CAPABILITY_IAM
+cat ${ROOT_DIR}/parameters/${STAGE}-app-endpoint | xargs aws cloudformation deploy --template-file ${ROOT_DIR}/cloudformation/app-endpoint.cf.yaml --capabilities CAPABILITY_IAM
